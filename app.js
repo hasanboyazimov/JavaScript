8f3fc5688ddca76cf58bee2a09b6738c8e1403b1
hasanjon1221 <azimovhasanjon1221@gmail.com>
2023-01-23 18:53:48 +0500

    test

---
+++ app.js
@@ -0,0 +1,49 @@
+// let ism = prompt("Ismingizni kiriting: ");
+
+// let firstChar = ism.charAt().toUpperCase();
+
+// let lastChars = ism.slice(1).toLocaleLowerCase();
+
+// let result = firstChar + lastChars
+
+
+
+
+
+// const results = [76, 20, 100, 70, 68, 120, 97, 88]
+
+// for (let i = 0; i < results.length; i++) {
+
+//     console.log("Sizning natijangiz: " + results[i])
+
+//     if (results[i] === 68) {
+//         break
+//     }
+// }
+
+// let result = 0
+
+// for (let i= 0; i < results.length; i++) {
+//     if (result < results[i]) {
+//         result = results[i]
+//     }
+//     else {
+//         continue
+//     }
+// }
+
+// console.log(result);
+
+//     let name = prompt("Ismingizni kiriting: ")
+//     let age = Number(prompt("Yoshingizni kiriting: "));
+//     let result = `Qadirli ${name}, siz ${2023 - age} yilda tug'ilgansiz`;
+//    prompt(result)
+
+// const ismlar = ['Ahror', 'Sardor', 'Doniyor']
+
+// let ism = prompt('Ism kiriting: ')
+// if ismlar.includes(ism)
+
+
+// gut hub
+console.log('test');
